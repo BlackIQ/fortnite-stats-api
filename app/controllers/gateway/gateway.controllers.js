ce4731219ca38b868d5b5aab722d9250362c109f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-03-06 02:13:12 +0330

    Gateway + Export

---
+++ app/controllers/gateway/gateway.controllers.js
@@ -0,0 +1,28 @@
+import { User, Role } from "$app/models/index.js";
+
+export const HANDLE = async (req, res) => {
+  const data = req.body;
+
+  try {
+    data.telegram_id = data.id;
+
+    const user = await User.findOne({ telegram_id: data.telegram_id });
+
+    if (!user) {
+      const role = await Role.findOne({ value: "user" });
+
+      await User.create({ ...data, role: role._id });
+
+      return res.status(200).send({ message: "Registered", user });
+    }
+
+    await User.findOneAndUpdate(
+      { telegram_id: data.telegram_id },
+      { $set: data }
+    );
+
+    return res.status(200).send({ message: "Exists", user });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
---
+++ app/controllers/index.js
@@ -0,0 +1,8 @@
+import * as Permission from "$app/controllers/permission/permission.controllers.js";
+import * as Favorite from "$app/controllers/favorite/favorite.controllers.js";
+import * as Gateway from "$app/controllers/gateway/gateway.controllers.js";
+import * as Stat from "$app/controllers/stat/stat.controllers.js";
+import * as Role from "$app/controllers/role/role.controllers.js";
+import * as User from "$app/controllers/user/user.controllers.js";
+
+export { Permission, Role, User, Favorite, Gateway, Stat };
