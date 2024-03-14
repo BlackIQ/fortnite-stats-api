b8be5900e12dd31e11a38c9a245b1e9a27b73e2f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-03-15 03:17:17 +0330

    Update to get user

--- app/controllers/stat/stat.controllers.js
+++ app/controllers/stat/stat.controllers.js
@@ -1,7 +1,8 @@
+import { Favorite } from "$app/models/index.js";
 import { API } from "$app/api/index.js";
 
 export const FETCH = async (req, res) => {
-  const { name } = req.query;
+  const { name, user } = req.query;
 
   try {
     const { data: response } = await API.get("stats/br/v2", {
@@ -10,7 +11,10 @@ export const FETCH = async (req, res) => {
       },
     });
 
+    const favorite = await Favorite.findOne({ fortnite_id: name, user });
+
     const data = response.data;
+    data.favorite = favorite;
 
     return res.status(200).send(data);
   } catch (error) {
