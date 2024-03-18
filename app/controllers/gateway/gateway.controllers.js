87f4a17b0788b5f421449e6bf0096832ba6b6da2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-03-18 05:55:16 +0330

    Fixing

--- app/controllers/gateway/gateway.controllers.js
+++ app/controllers/gateway/gateway.controllers.js
@@ -23,6 +23,7 @@ export const HANDLE = async (req, res) => {
 
     return res.status(200).send({ message: "Exists", user });
   } catch (error) {
+    console.log(error);
     return res.status(500).send({ message: error.message });
   }
 };
