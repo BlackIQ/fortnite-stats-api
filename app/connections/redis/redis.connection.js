59841fe6ae319613ae3cf0ae6084c0a9f7034be2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-10-02 15:52:36 +0330

    Fix

--- app/connections/redis/redis.connection.js
+++ app/connections/redis/redis.connection.js
@@ -2,12 +2,12 @@ import redis from "ioredis";
 
 import { databaseConfig } from "$app/config/index.js";
 
-const { redis } = databaseConfig;
+const { redis: redisConfig } = databaseConfig;
 
 const client = new redis({
-  host: redis.host,
-  port: redis.port,
-  password: redis.password,
+  host: redisConfig.host,
+  port: redisConfig.port,
+  password: redisConfig.password,
 });
 
 export default client;
